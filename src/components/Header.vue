ddc2fd50539bd5c1b8e408c9b36576d480ed2208
unknown <gunaorusty@gmail.com>
2023-10-03 15:11:16 +0800

    initialize header dropdowns

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,14 +14,14 @@
                 <span class="fs-7">Version 0.2.4</span>
             </div>
         </div>
-        <div class="wrapper-dropdowns d-flex justify-content-center align-items-center">
+        <div class="wrapper-dropdowns d-flex justify-content-center align-items-center gap-2">
             <div class="dropdown">
                 <a class="notification btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <font-awesome-icon :icon="faBell" class="fs-5" />
                 </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <div class="card dropdown-item p5">
-                        Test
+                <div class="notification-content dropdown-menu dropdown-menu-end p-0">
+                    <div class="card dropdown-item bg-transparent">
+                        <p class="text-light mt-4 mb-3 px-3">Notifications</p>
                     </div>
                 </div>
             </div>
@@ -29,9 +29,9 @@
                 <a class="theme btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <font-awesome-icon :icon="faMoon" class="fs-5" />
                 </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <div class="card dropdown-item p5">
-                        Test
+                <div class="theme-content dropdown-menu dropdown-menu-end p-0">
+                    <div class="card dropdown-item bg-transparent">
+                        <p class="text-light mt-4 mb-3 px-3">Theme</p>
                     </div>
                 </div>
             </div>
@@ -39,9 +39,9 @@
                 <a class="user p-1 px-2 btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="/user-profile.jpg" class="rounded-3" alt="" srcset="">
                 </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <div class="card dropdown-item p5">
-                        Test
+                <div class="user-content dropdown-menu dropdown-menu-end">
+                    <div class="card dropdown-item bg-transparent">
+                        <p class="text-light mt-4 mb-3 px-3">User</p>
                     </div>
                 </div>
             </div>
@@ -83,22 +83,69 @@
     .card-version svg,
     .card-version span,
     .dropdown a {
-        color: var(--inactive-icon);
+        color: #78829D !important;
     }
     .dropdown a {
         line-height: 0;
     }
+    .dropdown .notification-content {
+        width: 375px !important;
+    }
+    .dropdown .notification,
+    .dropdown .theme {
+        padding: 0.5rem 0.7rem;
+        height: 2.4rem;
+    }
+    .dropdown .notification:hover,
+    .dropdown .theme:hover,
+    .dropdown .notification.show,
+    .dropdown .theme.show {
+        background-color: #2b2b40 !important;
+        border-color: transparent !important;
+    }
     .system-version:hover p,
     .card-version:hover span,
     .card-version:hover svg,
-    .dropdown a:hover svg {
+    .dropdown a:hover svg,
+    .dropdown a.show {
         color: var(--bs-primary) !important;
+    }
+    .dropdown .user:active,
+    .dropdown .user.show {
+        border-color: transparent;
+        background-color: transparent;
     }
     .wrapper-dropdowns .dropdown .dropdown-toggle::after {
         display: none;
     }
     .user img {
-        height: 2rem;
-        width: 2rem;
+        height: 2.2rem;
+        width: 2.2rem;
+    }
+    .dropdown-menu {
+        background-color: var(--dropdown-dark-bg);
+        border-color: var(--dropdown-dark-bg);
+        visibility: hidden;
+        opacity: 0;
+        transform: translateY(calc(2.4rem + 0.8rem + 5px)) !important;
+        transition: all 0.3s ease !important;
+    }
+    .dropdown-menu.show {
+        animation:  dropdown-show-up 0.3s ease !important;
+        visibility: visible;
+        opacity: 1;
+    }
+    @keyframes dropdown-show-up {
+        0% {
+            margin-top: 0.75rem;
+        }
+        100% {
+            margin-top: 0;
+        }
+    }
+    .notification-content .card,
+    .theme-content .card,
+    .user-content .card {
+        border: none;
     }
 </style>
